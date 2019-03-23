45c787d617b971acca5ade5a7b606ee0ae9c58e5
Fodor Lóránt <fodor.lori@hotmail.com>
2019-03-23 19:35:09 +0200

    Basic notification service. Dashboard component, routing added.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SwPush, SwUpdate } from "@angular/service-worker";
+import { PushNotificationService } from './services/notifications/push-notification.service';
 
 @Component({
     selector: 'app-root',
@@ -7,7 +8,10 @@ import { SwPush, SwUpdate } from "@angular/service-worker";
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    constructor(private swUpdate: SwUpdate) { }
+    constructor(
+        private swUpdate: SwUpdate,
+        private asd: PushNotificationService
+    ) { }
 
     ngOnInit() {
         if (this.swUpdate.isEnabled) {
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,18 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { PushNotificationService } from './services/notifications/push-notification.service';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+// import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: true })
   ],
-  providers: [],
+  providers: [PushNotificationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
