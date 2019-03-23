3352e0c01a6ef08aff82e583a77d6aba87f04f21
Fodor Lóránt <fodor.lori@hotmail.com>
2019-03-23 19:00:28 +0200

    Client starter app.

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { SwPush, SwUpdate } from "@angular/service-worker";
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+    constructor(private swUpdate: SwUpdate) { }
+
+    ngOnInit() {
+        if (this.swUpdate.isEnabled) {
+            this.swUpdate.available.subscribe(() => {
+                if (confirm("New version available. Load New Version?")) {
+                    window.location.reload();
+                }
+            });
+        }
+    }
+}
